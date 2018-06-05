82889a661fbc84373d48c69bcc04de5a47843109
Meyer <jason.r.meyer@accenture.com>
2018-06-05 15:22:30 -0500

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  bungie: {
+    apiKey: '92dbdf3924964a6e945cc774e646588d',
+    apiUrl: 'https://www.bungie.net/Platform/',
+    authUrl: 'https://www.bungie.net/en/OAuth/Authorize',
+    clientId: '23391',
+  },production: false
 };
 
 /*
@@ -13,3 +18,6 @@ export const environment = {
  * because it will have performance impact when throw error
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+// ng serve --ssl --ssl-key "server.key" --ssl-cert "server.crt"
